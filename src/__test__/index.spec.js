9997b98127d494fe6eedb1c897e2f39470f4f7bc
Jonathan Grimes <jsg2021@gmail.com>
2018-10-15 17:24:27 -0500

    add test

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,8 +1,26 @@
 /* eslint-env jest */
-describe('Tests', () => {
+import iOSversion, {isIOS} from '../index';
 
-	test ('Show tests running...', () => {
-		expect(true).toBe(true);
+describe('iOS detection', () => {
+	beforeEach (() => {
+		delete navigator.platform;
+		delete navigator.appVersion;
 	});
 
+	test ('iOSversion', () => {
+		expect(() => iOSversion()).not.toThrow();
+		Object.defineProperty(navigator, 'platform', {configurable: true, value: 'iPhone'});
+		Object.defineProperty(navigator, 'appVersion', {configurable: true, value: 'OS 1_2_3'});
+		expect(iOSversion()).toEqual([1,2,3]);
+	});
+
+	test ('isIOS', () => {
+		expect(() => isIOS()).not.toThrow();
+		expect(isIOS()).toBe(false);
+
+		Object.defineProperty(navigator, 'platform', {configurable: true, value: 'iPhone'});
+		Object.defineProperty(navigator, 'appVersion', {configurable: true, value: 'OS 1_2'});
+
+		expect(isIOS()).toBe(true);
+	});
 });
--- src/index.js
+++ src/index.js
@@ -11,3 +11,5 @@ export default function iOSversion () {
 
 	return null;
 }
+
+export const isIOS = () => !!iOSversion();
