92b2abd78a90130ea73233f4775ecf04b8d190aa
Jonathan Grimes <jsg2021@gmail.com>
2021-02-22 12:44:01 -0600

    formatting

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,25 +1,37 @@
 /* eslint-env jest */
-import iOSversion, {isIOS} from '../index';
+import iOSversion, { isIOS } from '../index';
 
 describe('iOS detection', () => {
-	beforeEach (() => {
+	beforeEach(() => {
 		delete navigator.platform;
 		delete navigator.appVersion;
 	});
 
-	test ('iOSversion', () => {
+	test('iOSversion', () => {
 		expect(() => iOSversion()).not.toThrow();
-		Object.defineProperty(navigator, 'platform', {configurable: true, value: 'iPhone'});
-		Object.defineProperty(navigator, 'appVersion', {configurable: true, value: 'OS 1_2_3'});
-		expect(iOSversion()).toEqual([1,2,3]);
+		Object.defineProperty(navigator, 'platform', {
+			configurable: true,
+			value: 'iPhone',
+		});
+		Object.defineProperty(navigator, 'appVersion', {
+			configurable: true,
+			value: 'OS 1_2_3',
+		});
+		expect(iOSversion()).toEqual([1, 2, 3]);
 	});
 
-	test ('isIOS', () => {
+	test('isIOS', () => {
 		expect(() => isIOS()).not.toThrow();
 		expect(isIOS()).toBe(false);
 
-		Object.defineProperty(navigator, 'platform', {configurable: true, value: 'iPhone'});
-		Object.defineProperty(navigator, 'appVersion', {configurable: true, value: 'OS 1_2'});
+		Object.defineProperty(navigator, 'platform', {
+			configurable: true,
+			value: 'iPhone',
+		});
+		Object.defineProperty(navigator, 'appVersion', {
+			configurable: true,
+			value: 'OS 1_2',
+		});
 
 		expect(isIOS()).toBe(true);
 	});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 /**
  * @returns {number[]} iOS version or null of not iOS
  */
-export default function iOSversion () {
+export default function iOSversion() {
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
 		// supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
-		let v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+		let v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
 		//if the user is in "requested desktop" version won't match.
-		return v && [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+		return (
+			v && [
+				parseInt(v[1], 10),
+				parseInt(v[2], 10),
+				parseInt(v[3] || 0, 10),
+			]
+		);
 	}
 
 	return null;
